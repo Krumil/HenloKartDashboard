2a8e3a2b70d66f6943b5231b827aab6bffdc23c6
s.saletti <s.saletti@qzrstudio.com>
2024-05-28 14:44:51 +0200

    push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import uvicorn
+import os
 from fastapi import FastAPI
 from app.websocket import websocket_endpoint
 from config.db import database
@@ -35,7 +36,8 @@ async def health_check():
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 # import asyncio
 # import os
