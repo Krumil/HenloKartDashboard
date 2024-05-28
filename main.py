b7dce4184d107f2722d2fbc513c19bff18e83f81
s.saletti <s.saletti@qzrstudio.com>
2024-05-28 16:23:09 +0200

    changedport

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ async def health_check():
 
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 10000))
+    port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
 
 # import asyncio
