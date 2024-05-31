d042b9b3bed04e9903beac62726082b7271c2a7b
s.saletti <s.saletti@qzrstudio.com>
2024-05-31 09:45:13 +0200

    push

--- main.py
+++ main.py
@@ -2,12 +2,24 @@ import asyncio
 import uvicorn
 import os
 from fastapi import FastAPI
-from app.websocket import websocket_endpoint
+from fastapi.middleware.cors import CORSMiddleware
 from config.db import database
+from utils.helpers import serialize_record
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
 
 app = FastAPI()
 
-app.add_websocket_route("/ws", websocket_endpoint)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 async def startup_tasks():
@@ -24,6 +36,23 @@ async def shutdown():
     await database.disconnect()
 
 
+@app.get("/api/stats/tokens")
+async def get_all_token_stats():
+    query = """
+    SELECT
+        token_id,
+        COUNT(*) as total_races,
+        SUM(CASE WHEN winning_token_id = token_id THEN 1 ELSE 0 END) as total_wins,
+        SUM(bet_size) as total_bet_won
+    FROM race_results, unnest(participant_token_ids) as token_id
+    GROUP BY token_id
+    ORDER BY token_id;
+    """
+    results = await database.fetch_all(query)
+    # {"data": [serialize_record(dict(result)) for result in new_results]}
+    return {"data": [serialize_record(dict(result)) for result in results]}
+
+
 @app.get("/health")
 async def health_check():
     return {"status": "ok"}
@@ -31,4 +60,5 @@ async def health_check():
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    # uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="localhost", port=port)
